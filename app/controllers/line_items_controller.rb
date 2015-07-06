b94c27c996d746f8c47c854b2db91e05a1a0beee
Oleksandr Boiko <oleksandr.boiko.ua@gmail.com>
2015-07-06 12:24:17 +0300

    Added AJAX at add to cart btn. Added clicakable images

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -31,7 +31,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to store_path }
+        format.js { @current_item = @line_item }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
@@ -62,7 +63,7 @@ class LineItemsController < ApplicationController
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to store_path, notice: "removed product #{@pr_title} from the cart"}
+      format.html { redirect_to store_path }
       format.js
       format.json { head :ok }
     end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,8 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
+    @cart = current_cart
+
     update_view_counter
     update_visited_msg
   end
